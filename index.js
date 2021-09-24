9f51a9df7b773c91c177255ac588bfdee71cd3ca
Caio Duarte <caio1903@gmail.com>
2021-09-24 19:13:23 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+$.ajax({
+  type: "get",
+  url: "https://api.covid19api.com/summary",
+  success: function (response) {
+    console.log(response.Countries);
+
+    for (let i = 0; i < response.Countries.length; i++) {
+      let totalActive =
+        response.Countries[i].TotalConfirmed -
+        response.Countries[i].TotalRecovered;
+      let tablerow = `<tr> <td>${response.Countries[i].Country}</td>
+      <td>${response.Countries[i].TotalConfirmed}</td>
+      <td>${totalActive}</td>
+      <td>${response.Countries[i].TotalRecovered}</td>
+      <td>${response.Countries[i].TotalDeaths}</td>
+      </tr>`;
+      $("#tbody").append(tablerow);
+    }
+    $("#covidtable").DataTable();
+  },
+  error: function (error) {
+    console.log(error);
+  }
+});
